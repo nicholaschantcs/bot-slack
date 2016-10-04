5faa61071518cbc68040f691b55c0f8b172ad487
Hugo Cherchi <hugo.cherchi@recast.ai>
2016-10-04 16:57:32 +0200

    [FIX]

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
   recast: {
-    token: 'a8979267261b0fc21470f6b90e737720',
+    token: 'RECAST TOKEN',
     language: 'en',
   },
   slack: {
-    token: 'xoxb-87151515079-daAaicXFRUebDP1OThtLktkY',
+    token: 'SLACK BOT TOKEN',
   },
   port: 8080,
 }
